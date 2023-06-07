ff5edfeafd13cd5a6745f8f392ad7037a2508443
Gabriele <gabrieletuozzo@gmail.com>
2023-06-07 19:26:02 +0200

    added lambda function to process data from iot devices and store them in the DB

---
+++ src/process_data.py
@@ -0,0 +1,45 @@
+import boto3
+import datetime
+import json
+
+LOW = 10.0
+MEDIUM = 25.00
+HIGH = 45.00
+
+#def lambda_handler(event, contex):
+def test():
+    sqs = boto3.resource('sqs', endpoint_url='http://localhost:4566')
+    dynomdb = boto3.resource('dynamodb', endpoint_url="http://localhost:4566")
+
+    table = dynomdb.Table('Bins_Salerno')
+
+    queue = sqs.get_queue_by_name(QueueName='Bins_Salerno')
+    messages = []
+    while True:
+        response = queue.receive_messages(MaxNumberOfMessages=10, VisibilityTimeout=10, WaitTimeSeconds=10)
+        if response:
+            messages.extend(response)
+        for message in messages:
+            content = json.loads(message.body)
+            kg = float(content['kilograms'])
+            if(kg <= LOW):
+                level = 'LOW'
+            elif(kg <= MEDIUM):
+                level = 'MEDIUM'
+            elif(kg <= HIGH):
+                level = 'HIGH'
+            else:
+                level = 'TO BE EMPTIED'
+
+            item = {
+                'device_id': int(content['device_id']),
+                'latitude': content['latitude'],
+                'longitude': content['longitude'],
+                'trash_level': level
+            }
+            table.put_item(Item=item)
+            message.delete()
+        else:
+            break
+
+test()
