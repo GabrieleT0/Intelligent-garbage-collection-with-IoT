945699ec2601074c2a85e4c97eb32b26c5af7573
Gabriele <gabrieletuozzo@gmail.com>
2023-06-08 14:33:25 +0200

    added send to sns topic in case of measurement failure

--- IoTDevices.py
+++ IoTDevices.py
@@ -5,9 +5,12 @@ import json
 
 FAILURE_RATE = 0.08
 
-def lambda_handler(event,context):
+#def lambda_handler(event,context):
+def test():
     sqs = boto3.resource('sqs', endpoint_url='http://localhost:4566')
-
+    sns_client = boto3.client('sns',endpoint_url='http://localhost:4566')
+    topicArn = 'arn:aws:sns:us-east-2:000000000000:sensor_error'
+    
     #load info of our simulated IoT device from a JSON file
     with open('IoTDevices_info.json','r') as f:
         iot_devices = json.load(f)
@@ -17,12 +20,16 @@ def lambda_handler(event,context):
     for device in iot_devices:
         measure_date = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         if random.random() < FAILURE_RATE:
-            #TODO: send failure message to AWS-SNS
+            device_id = device['device_id']
+            error_message = '{"device_id": "%d","error_date": "%s"}' % (device_id,measure_date)
+            sns_client.publish(TopicArn=topicArn,Message=error_message,Subject='ERROR')
             print('IoT device error')
         else:
             kg = round(random.uniform(0.0,50.50),2)
             device_id = device['device_id']
             lat = device['latitude']
             long = device['longitude']
-            msg_body = '{"device_id: "%s","measure_date":"%s","latitude": "%s","longitude: "%s", "kilograms": "%s""}' % (device_id,measure_date,lat,long,kg)
-            queue.send_message(MesageBody=msg_body)
+            msg_body = '{"device_id": "%d","measure_date": "%s","latitude": "%s","longitude": "%s","kilograms": "%s"}' % (device_id,measure_date,lat,long,kg)
+            queue.send_message(MessageBody=msg_body)
+
+test()
