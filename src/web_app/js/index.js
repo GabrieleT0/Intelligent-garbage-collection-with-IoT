d4da021aadeedc009f2e223e09044acbe73bc3f1
GabrieleT0 <gabrieletuozzo@gmail.com>
2023-06-10 23:12:06 +0200

    inserted function to get IoT data from the RESTful API and started to build the map with bins

--- src/web_app/js/index.js
+++ src/web_app/js/index.js
@@ -7,13 +7,31 @@ async function getIoTData(){
 }
 
 function createMap(){
+    //Set the map center
     var map = L.map('map').setView([40.68252333266151, 14.770895359322255], 15);
+    //Load IoT devices cordinates and create a marker on the map for each of them
+    getIoTData().then(jsonData => {
+        jsonData;
+        for(var i = 0; i < jsonData.length; i++){
+            lat = jsonData[i].latitude
+            long = jsonData[i].longitude
+            var greenIcon = L.icon({
+                iconUrl: 'img/medium.png',
+                iconSize:     [30, 40], // size of the icon
+                shadowSize:   [50, 64], // size of the shadow
+                iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+                shadowAnchor: [4, 62],  // the same for the shadow
+                popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+            });
+            var marker = L.marker([lat, long],{icon: greenIcon}).addTo(map);
+            //Here we can personalize the popup on the marker when it is clicked
+            marker.bindPopup("<b>Ciao, sono un bidone!</b><br>Qui puoi inserire quanto sono pieno.");
+        }
+    });
+    //Lastly, render the map
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    }).addTo(map);
 }
 
-getIoTData().then(jsonData => {
-    jsonData;
-});
